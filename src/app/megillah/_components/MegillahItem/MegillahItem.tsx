7c5257411bf96f461c4ce0fc60dd2b5ab2081ced
codydil <cdilmanian@gmail.com>
2024-01-31 18:50:28 -0500

    Update file extensions for thumbnail images

--- src/app/megillah/_components/MegillahItem/MegillahItem.tsx
+++ src/app/megillah/_components/MegillahItem/MegillahItem.tsx
@@ -24,7 +24,7 @@ function MegillahItem({
   const title = `Issue #${megillah.issue} - ${formattedDate}`;
 
   const src = megillah.thumbnailPath
-    ? `${megillah.thumbnailPath}.png`
+    ? `${megillah.thumbnailPath}.JPG`
     : "/thumbnails/default.png";
 
   return (
