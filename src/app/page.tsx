2cd3d34a10f70d3e0b2037091532cd9bf88eaf84
codydil <cdilmanian@gmail.com>
2025-11-18 17:08:10 -0500

    Updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,9 +23,11 @@ export default async function Home() {
           </Link>
           <Link
             className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="/raffle"
+            href="https://branchesofbracha.com/"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <h3 className="text-2xl font-bold">Watch Raffle →</h3>
+            <h3 className="text-2xl font-bold">Branches For Bracha →</h3>
             <div className="text-lg">
               Donate as little as $1 for a chance to win a Rolex watch!
               Authorize a pre-donation from $1-$550, get a unique random number
@@ -47,7 +49,7 @@ export default async function Home() {
             className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
             href="/wedding"
           >
-            <h3 className="text-2xl font-bold">Weddings →</h3>
+            <h3 className="text-2xl font-bold">Wedding Pictures →</h3>
             <div className="text-lg">
               Explore beautiful weddings from our community. View galleries and
               celebrate with us.
